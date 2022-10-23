626a190dfebed80c2d26fe061b1848ba9763d842
Vitali Bedik <vitalya.bedik@gmail.com>
2022-10-23 17:15:55 +0300

    #2.10 - adding UI logic (editing) to MyListItem

--- src/components/UI/myListItem/index.jsx
+++ src/components/UI/myListItem/index.jsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 
 import { Input, Tag, Typography } from "antd";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { SaveOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 import styles from "./MyListItem.module.scss";
 
@@ -37,31 +37,46 @@ const ListItem = ({ item, showItemsActions }) => {
             </Text>
           </div>
         ) : (
-          <Input
-            ref={inputRef}
-            addonBefore={item.time}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            // onPressEnter{(e) => console.log(e.target.value)}
-            allowClear
-            style={{ display: "flex" }}
-          />
+          <div className={styles[`listItem__input-update`]}>
+            {" "}
+            <Input
+              compact
+              ref={inputRef}
+              addonBefore={item.time}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              // onPressEnter{(e) => console.log(e.target.value)}
+              allowClear
+            />
+          </div>
         )}
       </div>
       <div className={!showItemsActions && styles[`listItem__icon-visibility`]}>
-        <EditOutlined
-          onClick={() => {
-            if (edit) {
-              setText(text);
-            }
-            setEdit(!edit);
-            valid();
-          }}
-        />
-        <DeleteOutlined
-          onClick={() => dispatch(deleteNote(item.id))}
-          className={styles[`listItem__icon-delete`]}
-        />
+        <div>
+          {!edit ? (
+            <div>
+              <EditOutlined
+                onClick={() => {
+                  if (edit) {
+                    setText(text);
+                  }
+                  setEdit(!edit);
+                  valid();
+                }}
+              />
+              <DeleteOutlined
+                onClick={() => dispatch(deleteNote(item.id))}
+                className={styles[`listItem__icon-delete`]}
+              />
+            </div>
+          ) : (
+            <SaveOutlined
+              onClick={() => {
+                setEdit(!edit);
+              }}
+            />
+          )}
+        </div>
       </div>
     </>
   );
