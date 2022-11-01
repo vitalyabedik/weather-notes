66f8f0255205814ccea563895798c2487a24d166
Vitali Bedik <vitalya.bedik@gmail.com>
2022-11-01 21:01:24 +0300

    #3.5.3 - adding oneCall URL

--- src/config.js
+++ src/config.js
@@ -1,15 +1,18 @@
 // OPENWEATHER API
 const BASE_WEATHER_URL_OPENWEATHER = "https://api.openweathermap.org/data/2.5/";
 const BASE_GEOCODING_URL_OPENWEATHER = "http://api.openweathermap.org/geo/1.0/";
-const OPENWEATHER_API_KEY = `b9ac83fce506bd99363a07a936631208`;
 
 export const openWeatherAPI = {
   getCurrentWeatherByCoordinates(lat, lon) {
-    return `${BASE_WEATHER_URL_OPENWEATHER}weather?lat=${lat}&lon=${lon}&units=metric&appid=${OPENWEATHER_API_KEY}`;
+    return `${BASE_WEATHER_URL_OPENWEATHER}weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
   },
   getCurrentWeatherByCityName(city) {
-    return `${BASE_WEATHER_URL_OPENWEATHER}weather?q=${city}&units=metric&appid=${OPENWEATHER_API_KEY}`;
+    return `${BASE_WEATHER_URL_OPENWEATHER}weather?q=${city}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
   },
+  getForecast(lat, lon) {
+    return `${BASE_WEATHER_URL_OPENWEATHER}onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+  },
+
   // getDailyForecast(lat, lon) {
   //   return `${BASE_WEATHER_URL_OPENWEATHER}forecast/daily?lat=${lat}&lon=${lon}&cnt={cnt}&units=metric&appid=${OPENWEATHER_API_KEY}`;
   // },
@@ -17,9 +20,9 @@ export const openWeatherAPI = {
 
 export const geocodingAPI = {
   getPlaceNameByCoordinates(lon, lat) {
-    return `${BASE_GEOCODING_URL_OPENWEATHER}reverse?lat=${lat}&lon=${lon}&limit=1&appid=${OPENWEATHER_API_KEY}`;
+    return `${BASE_GEOCODING_URL_OPENWEATHER}reverse?lat=${lat}&lon=${lon}&limit=1&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
   },
   getCoordinatesByCity(city) {
-    return `${BASE_GEOCODING_URL_OPENWEATHER}direct?q=${city}&limit=1&appid=${OPENWEATHER_API_KEY}`;
+    return `${BASE_GEOCODING_URL_OPENWEATHER}direct?q=${city}&limit=1&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
   },
 };
