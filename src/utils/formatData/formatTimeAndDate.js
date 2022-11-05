c9143e7d674c5d502ecdc93d453e00eb7418f90e
Vitali Bedik <vitalya.bedik@gmail.com>
2022-11-05 19:56:45 +0300

    #4.0 - decomposing time and date data, adding format data

---
+++ src/utils/formatData/formatTimeAndDate.js
@@ -0,0 +1,27 @@
+import moment from "moment";
+
+export const formatHourMinute = "HH:mm";
+export const formatWeekdayDateMonthYear = "dddd, D MMMM YYYY";
+export const formatCalendarYearMonthDay = "YYYY-MM-DD";
+
+// dateAndTime
+
+export const setCurrentTime = (value) => moment(value);
+export const getCurrentDay = (timestamp) => moment.unix(timestamp);
+export const getCurrentDate = () => moment().format(formatWeekdayDateMonthYear);
+export const getCurrentTime = (value) => moment(value).format(formatHourMinute);
+
+// calendar
+export const setCalendarDate = () =>
+  moment().format(formatCalendarYearMonthDay);
+
+export const weekDaysNumberFormat = moment.updateLocale("en", {
+  week: {
+    dow: 1,
+    doy: 7,
+  },
+});
+
+export const weekDaysNameFormat = moment.updateLocale("en", {
+  weekdaysMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+});
