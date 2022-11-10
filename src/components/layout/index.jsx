4dc29d53302e5ea6739c20749e10f06fc9abcf17
Vitali Bedik <vitalya.bedik@gmail.com>
2022-11-10 20:12:13 +0300

    #4.4 - dynamic background (basic logic)

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 import styles from "./AppLayout.module.scss";
 
@@ -8,11 +9,22 @@ import Weather from "../weather";
 import CurrentEvent from "../currentEvent";
 
 const AppLayout = () => {
+  const [background, setBackground] = useState("clouds");
+  const weather = useSelector((state) => state.weather.data);
+
+  const weatherMain = weather?.current?.weather?.[0]?.main.toLowerCase();
+
   const [isDailyForecast, setIsDailyForecast] = useState(true);
 
   const changeForecastOption = () => {
     setIsDailyForecast(!isDailyForecast);
   };
+
+  useEffect(() => {
+    setBackground(weatherMain);
+    const backgroundLayout = `var(--background-image-${background})`;
+    document.body.style.setProperty("--background-image", backgroundLayout);
+  }, []);
 
   return (
     <div className={styles.wrapper}>
